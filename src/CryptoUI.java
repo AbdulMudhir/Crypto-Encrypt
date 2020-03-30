9c363bf98ccc12fd777bb1e907b5ac3fc71b0a2d
abdul mudhir <abdulmudhirss@gmail.com>
2020-03-30 20:35:36 +0100

    display password

--- src/CryptoUI.java
+++ src/CryptoUI.java
@@ -1,9 +1,22 @@
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.security.CryptoPrimitive;
+import java.security.Key;
+
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 public class CryptoUI implements ActionListener{
 
@@ -21,8 +34,12 @@ public class CryptoUI implements ActionListener{
     private JPanel stringToEncryptPanel;
     private JLabel stringToEncryptLabel;
     private JPanel bottomPanel;
+    private  Cipher cipher;
 
-    public static void main (String [] args){
+
+
+
+    public static void main (String [] args) throws NoSuchAlgorithmException, NoSuchPaddingException {
 
         try{
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -43,10 +60,12 @@ public class CryptoUI implements ActionListener{
 
         CryptoUI cryptoUI = new CryptoUI();
         cryptoUI.setupWindow();
+
+
     }
 
 
-    public void setupWindow(){
+    public void setupWindow() throws NoSuchPaddingException, NoSuchAlgorithmException {
 
         JFrame frame = new JFrame("Crypto UI");
 
@@ -54,12 +73,24 @@ public class CryptoUI implements ActionListener{
 
         frame.getContentPane().add(mainPanel);
 
+        cipher = Cipher.getInstance("AES");
 
-        encrpyedTextArea.addKeyListener(new KeyListener() {
+
+        stringToEncryptTextArea.addKeyListener(new KeyListener() {
 
             @Override
             public void keyTyped(KeyEvent e) {
-                System.out.println(encrpyedTextArea.getText());
+                if (decryptRadioButton.isSelected()){
+
+                }
+                else{
+
+
+                    encryptString();
+
+
+
+                }
             }
 
             @Override
@@ -73,14 +104,22 @@ public class CryptoUI implements ActionListener{
             }
         });
 
+        showPasswordRadioButton.addActionListener(this::actionPerformed);
+
+
+
         frame.setSize(600,600);
 
         frame.setVisible(true);
 
     }
 
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
+
         if (e.getSource() == decryptRadioButton){
             if (decryptRadioButton.isSelected()){
             System.out.println("Radio is on");
@@ -92,8 +131,27 @@ public class CryptoUI implements ActionListener{
 
 
 
+
+        }
+        // check if the show password radio was enabled if so, display the password
+        if (e.getSource() == showPasswordRadioButton && showPasswordRadioButton.isSelected()){
+
+            passwordField.setEchoChar((char) 0);
+        }
+        else{
+            passwordField.setEchoChar('*');
         }
     }
+    public void encryptString() {
+
+
+        System.out.println(passwordField.getPassword());
+
+        String data = stringToEncryptTextArea.getText();
+
+
+    }
+
 
 
 }
