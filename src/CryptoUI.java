54475c15079cd261786410581b7ab803699b8c7b
abdul mudhir <abdulmudhirss@gmail.com>
2020-03-30 15:46:41 +0100

    Initial commit

---
+++ src/CryptoUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class CryptoUI {
+
+
+    private JPanel mainPanel;
+
+    public static void main (String [] args){
+
+        JFrame frame = new JFrame("Crypto UI");
+
+        
+    }
+}
